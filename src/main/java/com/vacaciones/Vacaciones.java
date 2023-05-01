729c4ea44448e9f0dee09c0f083cbac6e5032785
julcaso <dell@DESKTOP-9K0I75S>
2023-05-01 13:27:09 -0400

    Commit del archivo Vacaciones.java

--- src/main/java/com/vacaciones/Vacaciones.java
+++ src/main/java/com/vacaciones/Vacaciones.java
@@ -12,6 +12,6 @@ package com.vacaciones;
 public class Vacaciones {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Mundo!");
     }
 }
