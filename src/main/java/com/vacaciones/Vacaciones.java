8cff2f9dd78fa2a5db78bc4cf8f834643a36f406
julcaso <dell@DESKTOP-9K0I75S>
2023-05-01 13:48:51 -0400

    Probando...

--- src/main/java/com/vacaciones/Vacaciones.java
+++ src/main/java/com/vacaciones/Vacaciones.java
@@ -12,6 +12,6 @@ package com.vacaciones;
 public class Vacaciones {
 
     public static void main(String[] args) {
-        System.out.println("Esta es una prueba de commit!");
+        System.out.println("Probando!");
     }
 }
