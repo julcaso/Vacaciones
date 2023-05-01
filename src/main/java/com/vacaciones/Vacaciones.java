50839662f54150a8083f6b31e8c24d14f2579b1a
julcaso <dell@DESKTOP-9K0I75S>
2023-05-01 13:57:03 -0400

    Nueva version del archivo

--- src/main/java/com/vacaciones/Vacaciones.java
+++ src/main/java/com/vacaciones/Vacaciones.java
@@ -12,6 +12,6 @@ package com.vacaciones;
 public class Vacaciones {
 
     public static void main(String[] args) {
-        System.out.println("Probando!");
+        System.out.println("Probando nueva version del archivo!");
     }
 }
